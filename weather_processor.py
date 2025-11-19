1a34e56b023b706dbe8711531d4ddec5db4e4991
katie <ksanders5728@gmail.com>
2025-11-19 10:29:16 -0600

    Weather proessor created - initial commit to branch

---
+++ weather_processor.py
@@ -0,0 +1,12 @@
+from scrape_weather import WeatherScraper
+
+"""
+Katie Sanders and Param Kotak.
+Processes user input and diplays weather data based on input
+2025-11-19
+"""
+
+class WeatherProcessor:
+    
+    def __init__(self):
+        weather = 
